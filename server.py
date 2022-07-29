b5cbc9152ba46e46d19516c6c1db8db7646e14a3
Edgar <57034089+Edw125@users.noreply.github.com>
2022-07-29 11:14:55 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import FastAPI, WebSocket
 from starlette.requests import Request
 from starlette.responses import HTMLResponse
@@ -9,29 +7,6 @@ from starlette.templating import Jinja2Templates
 app = FastAPI(title='Example')
 templates = Jinja2Templates(directory="templates")
 
-
-class ConnectionManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
-
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append(websocket)
-
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove(websocket)
-
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_json(message)
-
-    async def broadcast(self, message: str):
-        for connection in self.active_connections:
-            await connection.send_json(message)
-
-
-manager = ConnectionManager()
-
-
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
@@ -40,13 +15,12 @@ async def home(request: Request):
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     print('Accepting client connection...')
-    await manager.connect(websocket)
     count = 1
     while True:
         try:
             data = await websocket.receive_json()
             data.update({"Number": str(count)})
-            await manager.send_personal_message(data, websocket)
+            await websocket.send_json(data)
             count += 1
         except Exception as e:
             print('error:', e)
